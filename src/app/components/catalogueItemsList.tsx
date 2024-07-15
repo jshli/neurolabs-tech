7c6e065de2d66e2eab57bce9b04069c5974aea58
Josh Li <joshxli.io@gmail.com>
2024-07-15 12:42:24 +0100

    docs: add open api schema

--- src/app/components/catalogueItemsList.tsx
+++ src/app/components/catalogueItemsList.tsx
@@ -1,8 +1,17 @@
 import { useGetCatalogueItems } from "@/hooks/useGetCatalogueItems"
 
 export const CatalogueItemsList = () => {
-  const data = useGetCatalogueItems()
+  const { isPending, isError, error, data } = useGetCatalogueItems()
   console.log('data', data)
-  return <p>hello</p>
+
+  if (isPending) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return <p>{error}</p>
+  }
+
+  return <p></p>
 
 }
