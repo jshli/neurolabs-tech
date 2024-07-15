9b295c3f80c7ae134f3dfe4af5eaf6dd1fbda9cd
Josh Li <joshxli.io@gmail.com>
2024-07-15 16:08:11 +0100

    feat: add storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-'use client'
+"use client";
 import { CatalogueItemsList } from "./components";
+import { Container, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <main className=""><CatalogueItemsList /></main>
+    <main className="">
+      <Container>
+        <Typography variant="h5" component="h2" mb={2} mt={4}>
+          Catalogue
+        </Typography>
+        <CatalogueItemsList />
+      </Container>
+    </main>
   );
 }
