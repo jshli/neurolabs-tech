66b20aa64d1fbf31de067125ce86a5a8674976ad
Josh Li <joshxli.io@gmail.com>
2024-07-16 17:09:18 +0100

    test: add submit form tests

--- src/app/ir-tasks/[uuid]/components/SubmitImageForm/SubmitImageForm.test.tsx
+++ src/app/ir-tasks/[uuid]/components/SubmitImageForm/SubmitImageForm.test.tsx
@@ -8,9 +8,26 @@ import { server } from "@/mocks/node";
 const SubmitImageForm = composeStory(Default, Meta);
 
 server.listen();
+const fetchSpy = jest.spyOn(global, "fetch");
 
 test("the button is disabled if no images picked", async () => {
   await SubmitImageForm.load();
   render(<SubmitImageForm />);
   expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+});
+
+test("it fires the post request if file uploaded", async () => {
+  userEvent.setup();
+  global.URL.createObjectURL = jest.fn();
+
+  const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+  await SubmitImageForm.load();
+  render(<SubmitImageForm />);
+  await userEvent.upload(
+    screen.getByLabelText("Upload a new image to process"),
+    file,
+  );
+  userEvent.click(screen.getByRole("button", { name: "Submit" }));
+  waitFor(() => expect(fetchSpy).toHaveBeenCalled());
 });
